5b4b9d383dd94dd1b66b57cbb2e5556fa518f894
Starunov Andrei <ASStarunov@sberbank.ru>
2024-03-16 14:21:22 +0300

    init: - init project

---
+++ tables/app_tables.py
@@ -0,0 +1,47 @@
+import enum
+from datetime import datetime
+
+from sqlalchemy import Column, TIMESTAMP, Integer, ForeignKey, Text, VARCHAR, Enum
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class EnumCategories(enum.Enum):
+    """Перечисление списка категорий сайтов"""
+    blog = "blog"
+    shop = "shop"
+    social_net = "social_net"
+    other = "other"
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(VARCHAR)
+    login = Column(VARCHAR)
+    password = Column(VARCHAR)
+    created_at = Column(TIMESTAMP, default=datetime.utcnow)
+
+
+class Site(Base):
+    __tablename__ = 'sites'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(VARCHAR)
+    description = Column(VARCHAR, default=None)
+    url = Column(VARCHAR)
+    user_id = Column(Integer, ForeignKey(User.id))
+    category = Column(Enum(EnumCategories))
+    created_at = Column(TIMESTAMP, default=datetime.utcnow)
+
+
+class Comment(Base):
+    __tablename__ = "comments"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey(User.id))
+    site_id = Column(Integer, ForeignKey(Site.id))
+    text = Column(Text)
+    created_at = Column(TIMESTAMP, default=datetime.utcnow)
